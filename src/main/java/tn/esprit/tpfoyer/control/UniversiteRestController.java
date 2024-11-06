b19371552d7d89d7866718857f6b4fbcb755b82d
khawla798 <khaoula.addouli@esprit.tn>
2024-11-06 23:46:41 +0100

    modification pom

--- src/main/java/tn/esprit/tpfoyer/control/UniversiteRestController.java
+++ src/main/java/tn/esprit/tpfoyer/control/UniversiteRestController.java
@@ -12,53 +12,47 @@ import java.util.List;
 @RequestMapping("/universite")
 public class UniversiteRestController {
 
-    private final IUniversiteService universiteService;
+    IUniversiteService universiteService;
 
     // Récupérer toutes les universités
     @GetMapping("/retrieve-all-universites")
+    @ResponseBody
     public List<Universite> getUniversites() {
         return universiteService.retrieveAllUniversites();
     }
 
     // Récupérer une université par son ID
     @GetMapping("/retrieve-universite/{universite-id}")
+    @ResponseBody
     public Universite retrieveUniversite(@PathVariable("universite-id") Long uId) {
         return universiteService.retrieveUniversite(uId);
     }
 
     // Ajouter une nouvelle université
     @PostMapping("/add-universite")
+    @ResponseBody
     public Universite addUniversite(@RequestBody Universite u) {
         return universiteService.addUniversite(u);
     }
 
     // Supprimer une université par son ID
     @DeleteMapping("/remove-universite/{universite-id}")
+    @ResponseBody
     public void removeUniversite(@PathVariable("universite-id") Long uId) {
         universiteService.removeUniversite(uId);
     }
 
     // Modifier une université existante
     @PutMapping("/modify-universite")
+    @ResponseBody
     public Universite modifyUniversite(@RequestBody Universite u) {
         return universiteService.modifyUniversite(u);
     }
 
     // Compter le nombre total d'universités
     @GetMapping("/total-universites")
+    @ResponseBody
     public long totalUniversites() {
         return universiteService.calculateTotalUniversites();
-    }
-
-    // Récupérer les universités par localisation
-    @GetMapping("/retrieve-by-location/{location}")
-    public List<Universite> getUniversitesByLocation(@PathVariable("location") String location) {
-        return universiteService.findByLocation(location);
-    }
-
-    // Récupérer les universités par nom
-    @GetMapping("/retrieve-by-nom/{nom}")
-    public List<Universite> getUniversitesByNom(@PathVariable("nom") String nom) {
-        return universiteService.findByNomUniversite(nom);
     }
 }
