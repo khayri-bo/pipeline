15aa3417931e523114d68e78c3b438726126ec77
khawla798 <khaoula.addouli@esprit.tn>
2024-11-06 23:50:04 +0100

    modification pom

--- src/main/java/tn/esprit/tpfoyer/control/UniversiteRestController.java
+++ src/main/java/tn/esprit/tpfoyer/control/UniversiteRestController.java
@@ -16,42 +16,40 @@ public class UniversiteRestController {
 
     // Récupérer toutes les universités
     @GetMapping("/retrieve-all-universites")
-    @ResponseBody
     public List<Universite> getUniversites() {
         return universiteService.retrieveAllUniversites();
     }
 
     // Récupérer une université par son ID
     @GetMapping("/retrieve-universite/{universite-id}")
-    @ResponseBody
+
     public Universite retrieveUniversite(@PathVariable("universite-id") Long uId) {
         return universiteService.retrieveUniversite(uId);
     }
 
     // Ajouter une nouvelle université
     @PostMapping("/add-universite")
-    @ResponseBody
+
     public Universite addUniversite(@RequestBody Universite u) {
         return universiteService.addUniversite(u);
     }
 
     // Supprimer une université par son ID
     @DeleteMapping("/remove-universite/{universite-id}")
-    @ResponseBody
+
     public void removeUniversite(@PathVariable("universite-id") Long uId) {
         universiteService.removeUniversite(uId);
     }
 
     // Modifier une université existante
     @PutMapping("/modify-universite")
-    @ResponseBody
     public Universite modifyUniversite(@RequestBody Universite u) {
         return universiteService.modifyUniversite(u);
     }
 
     // Compter le nombre total d'universités
     @GetMapping("/total-universites")
-    @ResponseBody
+
     public long totalUniversites() {
         return universiteService.calculateTotalUniversites();
     }
