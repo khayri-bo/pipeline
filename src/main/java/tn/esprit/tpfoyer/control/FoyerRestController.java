a2e162b4d760740a16c88813cb3007f295d867f3
khawla798 <khaoula.addouli@esprit.tn>
2024-11-07 01:20:11 +0100

    modification test

--- src/main/java/tn/esprit/tpfoyer/control/FoyerRestController.java
+++ src/main/java/tn/esprit/tpfoyer/control/FoyerRestController.java
@@ -14,27 +14,22 @@ public class FoyerRestController {
 
     IFoyerService foyerService;
 
-    // http://localhost:8089/tpfoyer/foyer/retrieve-all-foyers
     @GetMapping("/retrieve-all-foyers")
     public List<Foyer> getFoyers() {
         List<Foyer> listFoyers = foyerService.retrieveAllFoyers();
         return listFoyers;
     }
-    // http://localhost:8089/tpfoyer/foyer/retrieve-foyer/8
     @GetMapping("/retrieve-foyer/{foyer-id}")
     public Foyer retrieveFoyer(@PathVariable("foyer-id") Long fId) {
         Foyer foyer = foyerService.retrieveFoyer(fId);
         return foyer;
     }
-
-    // http://localhost:8089/tpfoyer/foyer/add-foyer
     @PostMapping("/add-foyer")
     public Foyer addFoyer(@RequestBody Foyer f) {
         Foyer foyer = foyerService.addFoyer(f);
         return foyer;
     }
 
-    // http://localhost:8089/tpfoyer/foyer/remove-foyer/{foyer-id}
     @DeleteMapping("/remove-foyer/{foyer-id}")
     public void removeFoyer(@PathVariable("foyer-id") Long fId) {
         foyerService.removeFoyer(fId);
